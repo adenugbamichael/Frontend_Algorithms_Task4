2aaae3269c4882560edfd9829768332e760ae38b
adenugbamichael <adenugbamicke@gmail.com>
2021-05-22 23:58:36 +0100

    first commit

---
+++ script1.js
@@ -0,0 +1,16 @@
+//1. Write a function named "convertFahrToCelsius" that takes a single parameter and converts it to celsius.
+
+function convertFahrToCelsius(temp) {
+  if (typeof temp === 'number' || typeof temp === 'string') {
+    return (((Number(temp) - 32) * 5) / 9).toFixed(4);
+  } else {
+    return `${JSON.stringify(
+      temp
+    )} is not a valid number but a/an ${typeof temp}`;
+  }
+}
+
+console.log(convertFahrToCelsius(0));
+console.log(convertFahrToCelsius('0'));
+console.log(convertFahrToCelsius([1, 2, 3]));
+console.log(convertFahrToCelsius({ temp: 0 }));
